b62e649a42c1cc8372b85ec0a8493adcc4acaae6
ankurpowerplay <ankur.jar123@gmail.com>
2023-01-21 15:33:47 +0530

    Category updated

--- utils/constants.js
+++ utils/constants.js
@@ -1,11 +1,14 @@
 const CATEGORY = [
-    {id: "1", price: "500", val: 'LKG to 3 class art and craft'},
-    {id: "2", price: "600", val: '4 class to ,8 all subjects'},
-    {id: "3", price: "700", val: '9 class  to 12 class all subjects for ICSE/ CBSE'},
-    {id: "4", price: "800", val: 'B.com H-P/BA H-P/B.SlC/B.TECH/DIPLOMA/(HM)/ LAW all categories '},
-    {id: "5", price: "900", val: 'Office PPT (employee and students)'},
-    {id: "6", price: "1000", val: 'B.ed /LD'},
-    {id: "7", price: "1100", val: 'DLED'},
+    {id: "1", price: "599", val: 'LKG to 3rd class Art & Craft'},
+    {id: "2", price: "499",val: '4th to 8th class all subjects'},
+    {id: "3", price: "499",val: '9th to 12th class all subjects for ICSE / CBSE'},
+    {id: "4", price: "899",val: 'B.com H-P/BA H-P/B.SlC'},
+    {id: "5", price: "1999",val: 'B.TECH/DIPLOMA/(HM)'},
+    {id: "6", price: "899",val: 'Office PPT (Employees)'},
+    {id: "7", price: "499",val: 'Office PPT (Students)'},
+    {id: "8", price: "1999",val: 'B.ed project or assignment'},
+    {id: "9", price: "3999",val: 'B.ed learning design'},
+    {id: "10", price: "2999",val: 'LAW all categories'},
 ]
 
 module.exports = {
