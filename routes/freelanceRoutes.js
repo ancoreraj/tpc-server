0f3f8ac42a492e26944617ed5cc67246e8bf55c4
ankurpowerplay <ankur.jar123@gmail.com>
2023-07-02 14:21:23 +0530

    add: freelance update route

--- routes/freelanceRoutes.js
+++ routes/freelanceRoutes.js
@@ -100,4 +100,11 @@ router.post("/set-freelance", async (req, res) => {
     }
 })
 
+router.post('/update-freelance', ensureAuth, async (req, res) => {
+    
+    await UserModel.findByIdAndUpdate(req.user._id, req.body);
+
+    res.status(200).json({msg: "User Update"});
+});
+
 module.exports = router;
